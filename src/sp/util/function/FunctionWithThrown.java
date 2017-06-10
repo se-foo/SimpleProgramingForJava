a2e57dd17785909c2113a8784ce9e6b691c9e3ca
Se-foo <Se-foo@192.168.0.3>
2017-06-10 22:12:11 +0900

    Update FunctionWithThrown
    Initial FunctionWithThrown.OfInt

--- src/sp/util/function/FunctionWithThrown.java
+++ src/sp/util/function/FunctionWithThrown.java
@@ -12,6 +12,7 @@ package sp.util.function;
 import java.util.Objects;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
 import java.util.function.ToDoubleFunction;
 
 /**
@@ -457,6 +458,137 @@ public interface FunctionWithThrown<X extends Throwable> {
          * @return 変換後の関数.
          */
         default DoubleUnaryOperator toFunction() {
+            return this.toFunction(cause -> new RuntimeException(cause));
+        }
+    }
+
+    /**
+     * <p>
+     * 単一のオペランドに作用してオペランドと同じ型の結果を生成する演算を表す.
+     * </p>
+     * <p>
+     * これは, {@link #apply(int)} を関数メソッドに持つ関数型インタフェースである.
+     * </p>
+     *
+     * @author Se-foo
+     * @param <X>
+     *            評価中に発生するエラークラス.
+     * @since 0.1
+     */
+    @FunctionalInterface
+    static interface OfInt<X extends Throwable> extends FunctionWithThrown<X> {
+
+        /**
+         * 指定された引数にこの関数を適用する.
+         *
+         * @param target
+         *            入力引数.
+         * @return 関数の結果.
+         * @throws X
+         *             結果生成中にエラーが発生した場合.
+         */
+        int apply(int target) throws X;
+
+        /**
+         * まず入力に関数 before を適用し, 次に結果にこの関数を適用する合成関数を返す.
+         *
+         * @param before
+         *            この関数を適用する前に適用する関数.
+         * @return まず before 関数を適用し, 次にこの関数を適用する合成関数.
+         * @throws NullPointerException
+         *             before 関数が NULL の場合.
+         */
+        default FunctionWithThrown.OfInt<X> compose(FunctionWithThrown.OfInt<? extends X> before) {
+            Objects.requireNonNull(before);
+            return target -> this.apply(before.apply(target));
+        }
+
+        /**
+         * まず入力に関数 before を適用し, 次に結果にこの関数を適用する合成関数を返す.
+         *
+         * @param before
+         *            この関数を適用する前に適用する関数.
+         * @return まず before 関数を適用し, 次にこの関数を適用する合成関数.
+         * @throws NullPointerException
+         *             before 関数が NULL の場合.
+         */
+        default FunctionWithThrown.OfInt<X> composeFunction(IntUnaryOperator before) {
+            Objects.requireNonNull(before);
+            return target -> this.apply(before.applyAsInt(target));
+        }
+
+        /**
+         * まず入力にこの関数を適用し, 次に結果に関数 after を適用する合成関数を返す.
+         *
+         * @param after
+         *            この関数を適用した後で適用する関数.
+         * @return まずこの関数を適用し, 次に after 関数を適用する合成関数.
+         * @throws NullPointerException
+         *             after 関数が NULL の場合.
+         */
+        default FunctionWithThrown.OfInt<X> andThen(FunctionWithThrown.OfInt<? extends X> after) {
+            Objects.requireNonNull(after);
+            return target -> after.apply(this.apply(target));
+        }
+
+        /**
+         * まず入力にこの関数を適用し, 次に結果に関数 after を適用する合成関数を返す.
+         *
+         * @param after
+         *            この関数を適用した後で適用する関数.
+         * @return まずこの関数を適用し, 次に after 関数を適用する合成関数.
+         * @throws NullPointerException
+         *             after 関数が NULL の場合.
+         */
+        default FunctionWithThrown.OfInt<X> andThenFunction(IntUnaryOperator after) {
+            Objects.requireNonNull(after);
+            return target -> after.applyAsInt(this.apply(target));
+        }
+
+        /**
+         * <p>
+         * {@link java.util.function.IntUnaryOperator} に変換する.
+         * </p>
+         * <p>
+         * 発生するエラー又は非チェック例外はそのままスローされる. チェック例外又は左記以外の {@link Throwable} は
+         * 非チェック例外生成関数 throwable を呼び出し, その結果がスローされる.
+         * </p>
+         *
+         * @param throwable
+         *            非チェック例外生成関数.
+         * @return 変換後の関数.
+         * @throws NullPointerException
+         *             指定された非チェック例外生成関数が NULL, 又は生成された例外が NULL の場合.
+         */
+        default IntUnaryOperator toFunction(Function<? super Throwable, ? extends RuntimeException> throwable) {
+            Objects.requireNonNull(throwable);
+            return target -> {
+                try {
+                    return this.apply(target);
+                } catch (RuntimeException e) {
+                    throw e;
+                } catch (Exception e) {
+                    throw Objects.requireNonNull(throwable.apply(e));
+                } catch (Error e) {
+                    throw e;
+                } catch (Throwable e) {
+                    throw Objects.requireNonNull(throwable.apply(e));
+                }
+            };
+        }
+
+        /**
+         * <p>
+         * {@link java.util.function.IntUnaryOperator} に変換する.
+         * </p>
+         * <p>
+         * 発生するエラー又は非チェック例外はそのままスローされる. チェック例外又は左記以外の {@link Throwable} は
+         * {@link RuntimeException} でラッピングされてスローされる.
+         * </p>
+         *
+         * @return 変換後の関数.
+         */
+        default IntUnaryOperator toFunction() {
             return this.toFunction(cause -> new RuntimeException(cause));
         }
     }
