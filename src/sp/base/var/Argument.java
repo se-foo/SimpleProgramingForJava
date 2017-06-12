8fb90a2c39a6c1b918107b91cc3a3d0c881080ac
Se-foo <Se-foo@192.168.0.3>
2017-06-12 16:13:37 +0900

    Add Argument.OfDouble
    Initial Argument.OfDouble.

--- src/sp/base/var/Argument.java
+++ src/sp/base/var/Argument.java
@@ -11,11 +11,16 @@ package sp.base.var;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.DoubleFunction;
+import java.util.function.DoublePredicate;
+import java.util.function.DoubleSupplier;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 
 import sp.base.NonNullReturnValue;
+import sp.util.function.FunctionWithThrown;
 import sp.util.function.PredicateWithThrown;
 
 /**
@@ -170,6 +175,7 @@ public interface Argument {
          * @see #requireNonNull(Supplier)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> requireNonNull(CharSequence message) {
             return this.requireNonNull(() -> message);
         }
@@ -219,6 +225,7 @@ public interface Argument {
          * @see #requireInstanceOf(Class, Supplier)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> requireInstanceOf(Class<?> classObject) {
 
             // throw NullPointerException if classObject is null at
@@ -248,6 +255,7 @@ public interface Argument {
          * @see #requireInstanceOf(Class, Supplier)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> requireInstanceOf(Class<?> classObject, CharSequence message) {
 
             // throw NullPointerException if classObject is null at
@@ -277,6 +285,7 @@ public interface Argument {
          * @see #require(Predicate, Function)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> requireInstanceOf(Class<?> classObject, Supplier<? extends CharSequence> message) {
             Objects.requireNonNull(classObject);
             Objects.requireNonNull(message);
@@ -290,13 +299,15 @@ public interface Argument {
          * チェック対象が NULL でないことを確認する.
          * </p>
          * <p>
-         * チェック対象が NULL の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE を返す.
+         * チェック対象が NULL の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE
+         * を返す.
          * </p>
          *
          * @return 本チェックストリーム.
          * @see #filter(sp.util.function.PredicateWithThrown.OfObj)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> nonNull() {
             return this.filter(target -> Objects.nonNull(target));
         }
@@ -319,11 +330,472 @@ public interface Argument {
          * @see #filter(sp.util.function.PredicateWithThrown.OfObj)
          * @since 0.1
          */
+        @NonNullReturnValue
         default Argument.OfObj<T> instanceOf(Class<?> classObject) {
             Objects.requireNonNull(classObject);
 
             return this.filter(target -> classObject.isInstance(target));
         }
+
+        /**
+         * マッピングによるオブジェクト引数の前提条件の変換機構.
+         *
+         * @author Se-foo
+         * @param <T>
+         *            マッピング変換元のチェック対象クラス.
+         * @since 0.1
+         */
+        static interface Mappper<T> extends Mapper.FromObj<T, Argument> {
+
+            /**
+             * <p>
+             * 指定されたマッピングを用いてチェック対象を変換する.
+             * </p>
+             * <p>
+             * 変換中にエラーが発生した場合, 本メソッド以降のフィルタ・マッピング処理は無視され,
+             * {@link Argument#check() check()} は FALSE を返す.
+             * </p>
+             *
+             * @param <A>
+             *            マッピングの変換先クラス.
+             * @param <X>
+             *            変換中に発生するエラークラス.
+             * @param mapping
+             *            チェック対象の変換に用いるマッピング.
+             * @return チェック対象変換後のチェックストリーム.
+             * @throws NullPointerException
+             *             チェック対象の変換に用いるマッピングが NULL の場合.
+             * @throws X
+             *             変換中にエラーが発生した場合.
+             * @since 0.1
+             */
+            @Override
+            <A, X extends Throwable> Argument.OfObj<A> toObj(
+                    FunctionWithThrown.OfObj<? super T, ? extends A, ? extends X> mapping) throws X;
+
+            /**
+             * <p>
+             * チェック対象をキャストする.
+             * </p>
+             * <p>
+             * チェック対象が指定されたクラスを継承していない場合, 本メソッド以降のフィルタ・マッピング処理は無視され,
+             * {@link #check()} は FALSE を返す.
+             * </p>
+             *
+             * @param <A>
+             *            マッピングの変換先クラス.
+             * @param classObject
+             *            クラス情報.
+             * @return キャスト後のチェックストリーム.
+             * @throws NullPointerException
+             *             クラス情報が NULL の場合.
+             * @see #instanceOf(Class)
+             * @since 0.1
+             */
+            <A> Argument.OfObj<A> toCast(Class<? extends A> classObject);
+
+            /**
+             * <p>
+             * 指定されたマッピングを用いてチェック対象を変換する.
+             * </p>
+             * <p>
+             * 変換中にエラーが発生した場合, 本メソッド以降のフィルタ・マッピング処理は無視され,
+             * {@link Argument#check() check()} は FALSE を返す.
+             * </p>
+             *
+             * @param <X>
+             *            変換中に発生するエラークラス.
+             * @param mapping
+             *            チェック対象の変換に用いるマッピング.
+             * @return チェック対象変換後のチェックストリーム.
+             * @throws NullPointerException
+             *             チェック対象の変換に用いるマッピングが NULL の場合.
+             * @throws X
+             *             変換中にエラーが発生した場合.
+             * @since 0.1
+             */
+            @Override
+            <X extends Throwable> Argument.OfDouble toDouble(
+                    FunctionWithThrown.OfObjToDouble<? super T, ? extends X> mapping) throws X;
+
+            /**
+             * <p>
+             * 指定されたマッピングを用いてチェック対象を変換する.
+             * </p>
+             * <p>
+             * 変換中にエラーが発生した場合, 本メソッド以降のフィルタ・マッピング処理は無視され,
+             * {@link Argument#check() check()} は FALSE を返す.
+             * </p>
+             *
+             * @param <X>
+             *            変換中に発生するエラークラス.
+             * @param mapping
+             *            チェック対象の変換に用いるマッピング.
+             * @return チェック対象変換後のチェックストリーム.
+             * @throws NullPointerException
+             *             チェック対象の変換に用いるマッピングが NULL の場合.
+             * @throws X
+             *             変換中にエラーが発生した場合.
+             * @since 0.1
+             */
+            @Override
+            <X extends Throwable> Argument toInt(FunctionWithThrown.OfObjToInt<? super T, ? extends X> mapping)
+                    throws X;
+
+            /**
+             * チェック対象をハッシュ値に変換する.
+             *
+             * @implSpec ハッシュ値変換には {@link Objects#hashCode(Object)} を用いている.
+             * @return チェック対象変換後のチェックストリーム.
+             * @see #mapToInt(ToIntFunction)
+             * @since 0.1
+             */
+            default Argument toHash() {
+                return this.toInt(target -> Objects.hashCode(target));
+            }
+
+            /**
+             * <p>
+             * 指定されたマッピングを用いてチェック対象を変換する.
+             * </p>
+             * <p>
+             * 変換中にエラーが発生した場合, 本メソッド以降のフィルタ・マッピング処理は無視され,
+             * {@link Argument#check() check()} は FALSE を返す.
+             * </p>
+             *
+             * @param <X>
+             *            変換中に発生するエラークラス.
+             * @param mapping
+             *            チェック対象の変換に用いるマッピング.
+             * @return チェック対象変換後のチェックストリーム.
+             * @throws NullPointerException
+             *             チェック対象の変換に用いるマッピングが NULL の場合.
+             * @throws X
+             *             変換中にエラーが発生した場合.
+             * @since 0.1
+             */
+            @Override
+            <X extends Throwable> Argument toLong(FunctionWithThrown.OfObjToLong<? super T, ? extends X> mapping)
+                    throws X;
+        }
+
+        /**
+         * チェック対象の変換機構を取得する.
+         *
+         * @return チェック対象の変換機構.
+         * @since 0.1
+         */
+        Argument.OfObj.Mappper<T> map();
+    }
+
+    /**
+     * double 引数の前提条件.
+     *
+     * @author Se-foo
+     * @since 0.1
+     */
+    static interface OfDouble extends Argument {
+
+        /**
+         * <p>
+         * チェック対象を取得する.
+         * </p>
+         * <p>
+         * チェック対象がチェック内容に沿っていない場合, 指定された値 other を返す.
+         * </p>
+         *
+         * @param other
+         *            チェック対象がチェック内容に沿っていない場合の値.
+         * @return チェック対象又は other.
+         * @throws NullPointerException
+         *             チェック対象がチェック内容に沿っていない場合の値が NULL の場合.
+         * @see #orElseGet(Supplier)
+         * @since 0.1
+         */
+        default double orElse(double other) {
+            return this.orElseGet(() -> other);
+        }
+
+        /**
+         * <p>
+         * チェック対象を取得する.
+         * </p>
+         * <p>
+         * チェック対象がチェック内容に沿っていない場合, 戻り値生成関数 other を呼び出し, その結果を返す.
+         * </p>
+         *
+         * @param other
+         *            チェック対象がチェック内容に沿っていない場合の戻り値生成関数.
+         * @return チェック対象又は戻り値生成関数 other の呼び出し結果.
+         * @throws NullPointerException
+         *             チェック対象がチェック内容に沿っていない場合の戻り値生成関数が NULL, 又は戻り値生成関数の呼び出し結果が
+         *             NULL の場合.
+         * @see #check()
+         * @since 0.1
+         */
+        double orElseGet(DoubleSupplier other);
+
+        /**
+         * <p>
+         * チェック対象が指定されたチェック内容に沿っているか確認する.
+         * </p>
+         * <p>
+         * チェック内容に沿っていない場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE
+         * を返す.
+         * </p>
+         *
+         * @param <X>
+         *            チェック中に発生する例外クラス.
+         * @param filter
+         *            チェック内容.
+         * @return 本チェックストリーム.
+         * @throws NullPointerException
+         *             チェック内容が NULL の場合.
+         * @throws X
+         *             チェック中にエラーが発生した場合.
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        <X extends Throwable> Argument.OfDouble filter(PredicateWithThrown.OfDouble<? extends X> filter) throws X;
+
+        /**
+         * <p>
+         * チェック対象がチェック内容に沿っているか確認する.
+         * </p>
+         * <p>
+         * チェック内容に沿っていない場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE
+         * を返す.
+         * </p>
+         *
+         * @param <X>
+         *            チェック対象がチェック内容に沿っていない場合の例外クラス.
+         * @param predicate
+         *            チェック内容.
+         * @param throwable
+         *            チェック対象がチェック内容に沿っていない場合にスローする例外生成関数.
+         * @return 本チェックストリーム.
+         * @throws NullPointerException
+         *             チェック内容及びエラー生成関数が NULL, 又はエラー生成関数の呼び出し結果が NULL の場合.
+         * @throws X
+         *             チェック対象がチェック内容に沿っていない場合.
+         * @see #filter(sp.util.function.PredicateWithThrown.OfObj)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default <X extends Throwable> Argument.OfDouble require(DoublePredicate predicate,
+                DoubleFunction<? extends X> throwable) throws X {
+            Objects.requireNonNull(predicate);
+            Objects.requireNonNull(throwable);
+
+            return this.filter(target -> {
+                if (predicate.test(target)) {
+                    return true;
+                } else {
+                    throw Objects.requireNonNull(throwable.apply(target));
+                }
+            });
+        }
+
+        /**
+         * <p>
+         * チェック対象が負でないことを確認する.
+         * </p>
+         * <p>
+         * チェック対象が負の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE を返す.
+         * </p>
+         *
+         * @return 本チェックストリーム.
+         * @throws IllegalArgumentException
+         *             チェック対象が負の場合.
+         * @see #requireNonNegative(Supplier)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireNonNegative() {
+            return this.requireNonNegative(() -> null);
+        }
+
+        /**
+         * <p>
+         * チェック対象が負でないことを確認する.
+         * </p>
+         * <p>
+         * チェック対象が負の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE を返す.
+         * </p>
+         *
+         * @param message
+         *            チェック対象が NULL の場合のエラーメッセージ.
+         * @return 本チェックストリーム.
+         * @throws IllegalArgumentException
+         *             チェック対象が負の場合.
+         * @see #requireNonNegative(Supplier)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireNonNegative(CharSequence message) {
+            return this.requireNonNegative(() -> message);
+        }
+
+        /**
+         * <p>
+         * チェック対象が負でないことを確認する.
+         * </p>
+         * <p>
+         * チェック対象が負の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE を返す.
+         * </p>
+         *
+         * @param message
+         *            チェック対象が負の場合のエラーメッセージ生成関数.
+         * @return 本チェックストリーム.
+         * @throws NullPointerException
+         *             エラーメッセージ生成関数が NULL の場合.
+         * @throws IllegalArgumentException
+         *             チェック対象が負の場合.
+         * @see #require(DoublePredicate, DoubleFunction)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireNonNegative(Supplier<? extends CharSequence> message) {
+            Objects.requireNonNull(message);
+
+            return this.require(target -> target >= 0, target -> new IllegalArgumentException(
+                    Optional.ofNullable(message.get()).map(m -> m.toString()).orElse(null)));
+        }
+
+        /**
+         * <p>
+         * チェック対象が始端 (from, これを含む) と終端 (to, これを含まない) の間であることを確認する.
+         * </p>
+         * <p>
+         * チェック対象が指定された範囲外の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は
+         * FALSE を返す.
+         * </p>
+         *
+         * @param from
+         *            始端. (これを含む)
+         * @param to
+         *            終端. (これを含まない)
+         * @return 本チェックストリーム.
+         * @throws IllegalArgumentException
+         *             指定された始端が終端より大きい場合, 又はチェック対象がチェック対象が指定された範囲外の場合.
+         * @see #requireBounds(double, double, Supplier)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireBounds(double from, double to) {
+
+            // throw IllegalArgumentException if from > to at #requireBounds.
+            return this.requireBounds(from, to, () -> null);
+        }
+
+        /**
+         * <p>
+         * チェック対象が始端 (from, これを含む) と終端 (to, これを含まない) の間であることを確認する.
+         * </p>
+         * <p>
+         * チェック対象が指定された範囲外の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は
+         * FALSE を返す.
+         * </p>
+         *
+         * @param from
+         *            始端. (これを含む)
+         * @param to
+         *            終端. (これを含まない)
+         * @param message
+         *            チェック対象が指定された範囲外の場合のエラーメッセージ.
+         * @return 本チェックストリーム.
+         * @throws IllegalArgumentException
+         *             指定された始端が終端より大きい場合, 又はチェック対象がチェック対象が指定された範囲外の場合.
+         * @see #requireBounds(double, double, Supplier)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireBounds(double from, double to, CharSequence message) {
+
+            // throw IllegalArgumentException if from > to at #requireBounds.
+            return this.requireBounds(from, to, () -> message);
+        }
+
+        /**
+         * <p>
+         * チェック対象が始端 (from, これを含む) と終端 (to, これを含まない) の間であることを確認する.
+         * </p>
+         * <p>
+         * チェック対象が指定された範囲外の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は
+         * FALSE を返す.
+         * </p>
+         *
+         * @param from
+         *            始端. (これを含む)
+         * @param to
+         *            終端. (これを含まない)
+         * @param message
+         *            チェック対象が指定された範囲外の場合のエラーメッセージ生成関数.
+         * @return 本チェックストリーム.
+         * @throws NullPointerException
+         *             エラーメッセージ生成関数が NULL の場合.
+         * @throws IllegalArgumentException
+         *             指定された始端が終端より大きい場合, 又はチェック対象がチェック対象が指定された範囲外の場合.
+         * @see #require(DoublePredicate, DoubleFunction)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble requireBounds(double from, double to, Supplier<? extends CharSequence> message) {
+            if (from > to) {
+                throw new IllegalArgumentException();
+            }
+            Objects.requireNonNull(message);
+
+            return this.require(target -> target >= from && target < to, target -> new IllegalArgumentException(
+                    Optional.ofNullable(message.get()).map(m -> m.toString()).orElse(null)));
+        }
+
+        /**
+         * <p>
+         * チェック対象が負でないことを確認する.
+         * </p>
+         * <p>
+         * チェック対象が負の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は FALSE を返す.
+         * </p>
+         *
+         * @return 本チェックストリーム.
+         * @see #filter(sp.util.function.PredicateWithThrown.OfDouble)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble nonNegative() {
+            return this.filter(target -> target >= 0);
+        }
+
+        /**
+         * <p>
+         * チェック対象が始端 (from, これを含む) と終端 (to, これを含まない) の間であることを確認する.
+         * </p>
+         * <p>
+         * チェック対象が指定された範囲外の場合, 本メソッド以降のフィルタ・マッピング処理は無視され, {@link #check()} は
+         * FALSE を返す.
+         * </p>
+         *
+         * @param from
+         *            始端. (これを含む)
+         * @param to
+         *            終端. (これを含まない)
+         * @return 本チェックストリーム.
+         * @throws IllegalArgumentException
+         *             指定された始端が終端より大きい場合.
+         * @see #filter(sp.util.function.PredicateWithThrown.OfDouble)
+         * @since 0.1
+         */
+        @NonNullReturnValue
+        default Argument.OfDouble isBounds(double from, double to) {
+            if (from > to) {
+                throw new IllegalArgumentException();
+            }
+
+            return this.filter(target -> target >= from && target < to);
+        }
+
+        // TODO : need #map()
     }
 
     /**
