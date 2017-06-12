db14ff3761fe135503b8d1340e92268aa86f7d5b
Se-foo <Se-foo@192.168.0.3>
2017-06-12 14:08:38 +0900

    Add Variable
    Initial Variable.

---
+++ src/sp/base/var/Variable.java
@@ -0,0 +1,39 @@
+/*
+sp.base.var.Variable
+
+Copyright (c) 2017 Se-foo
+
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+*/
+package sp.base.var;
+
+/**
+ * 変数の前提条件記述のファクトリークラス.
+ *
+ * @author Se-foo
+ * @since 0.1
+ */
+public final class Variable {
+
+    /**
+     * 指定された引数オブジェクトの前提条件を作成する.
+     *
+     * @param <T>
+     *            指定された引数クラス.
+     * @param target
+     *            引数.
+     * @return 引数の前提条件.
+     */
+    public static <T> Argument.OfObj<T> arg(T target) {
+        // TODO : Implement Argument.OfObj.
+        return null;
+    }
+
+    /**
+     * Constractor.
+     */
+    Variable() {
+        super();
+    }
+}
